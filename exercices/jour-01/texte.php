6bab4c426c42de3a2a3a5465d32aae66260cfd51
dylanholin-campus <dylan.holin@le-campus-numerique.fr>
2026-01-05 14:06:15 +0100

    Jour 01: exercices PHP

---
+++ exercices/jour-01/texte.php
@@ -0,0 +1,18 @@
+<?php
+
+$brand = "Nike";
+$model = "Air Max";
+
+// 1) Concaténation (.)
+echo "Chaussures " . $brand . " " . $model;
+
+// 2) Interpolation ("...")
+echo "Chaussures $brand $model";
+
+// 3) sprintf()
+echo sprintf("Chaussures %s %s", $brand, $model);
+
+// Test : différence "..." vs '...'
+$price = 99.99;
+echo "Prix : $price €";  // Que s'affiche-t-il ? Affiche : Prix : 99.99 €
+echo 'Prix : $price €';  // Et là ? Affiche : Prix : $price €
---
+++ exercices/jour-01/types.php
@@ -0,0 +1,4 @@
+<?php
+$price = "29.99€";
+$result = $price + 10;
+var_dump($result);
