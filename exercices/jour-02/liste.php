98487227c3e69a6f176b65f4c7c272327fd1b3ba
dylanholin-campus <dylan.holin@le-campus-numerique.fr>
2026-01-07 15:48:51 +0100

    Jour 04: exo 6 + fix-jour2-liste

--- exercices/jour-02/liste.php
+++ exercices/jour-02/liste.php
@@ -1,9 +1,10 @@
 <?php
 $groceries = ["Pomme", "Banane", "Chocolat", "cerise", "RTX 5080"];
 array_push($groceries, "Raptor", "Tourte");
+$groceries = array_values($groceries);
 unset($groceries[2]);
+$groceries = array_values($groceries);
 echo " Premier article : " . $groceries[0];
 echo " Dernier article : " . $groceries[count($groceries) - 1];
 echo " Nombre total d'articles : " . count($groceries);
-$groceries = array_values($groceries);
 var_dump($groceries);
