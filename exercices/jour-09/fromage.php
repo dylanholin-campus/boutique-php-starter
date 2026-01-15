4bf5cef30cc8988caad019a4f1ed82d9fe37b548
dylanholin-campus <dylan.holin@le-campus-numerique.fr>
2026-01-15 10:12:19 +0100

    Jour 09: exo 2

---
+++ exercices/jour-09/fromage.php
@@ -0,0 +1,117 @@
+<?php
+
+class Category
+{
+    public $nom;
+
+    public function __construct($nom)
+    {
+        $this->nom = $nom;
+    }
+}
+
+class Product
+{
+    public $nom;
+    public $prix;
+    public $category;
+
+    public function __construct($nom, $prix, $category)
+    {
+        $this->nom = $nom;
+        $this->prix = $prix;
+        $this->category = $category;
+    }
+}
+
+class quantity
+{
+    public $product;
+    public $quantite;
+
+    public function __construct($product, $quantite)
+    {
+        $this->product = $product;
+        $this->quantite = max(0, (int)$quantite);
+    }
+
+    public function getTotal()
+    {
+        return $this->product->prix * $this->quantite;
+    }
+
+    public function incrementequantity($pas = 1)
+    {
+        $this->quantite += max(0, (int)$pas);
+    }
+
+    public function decrementequantity($pas = 1)
+    {
+        $this->quantite = max(0, $this->quantite - max(0, (int)$pas));
+    }
+}
+
+class weight
+{
+    public $product;
+    public $weight;
+
+    public function __construct($product, $weight)
+    {
+        $this->product = $product;
+        $this->weight = max(0, (float)$weight);
+    }
+
+    public function getTotalweight()
+    {
+        return $this->product->prix * $this->weight;
+    }
+
+    public function incrementeweight($pas = 1)
+    {
+        $this->weight += max(0, (float)$pas);
+    }
+
+    public function decrementeweight($pas = 1)
+    {
+        $this->weight = max(0, $this->weight - max(0, (float)$pas));
+    }
+}
+
+$categorieFromages = new Category("Fromages");
+
+$produitBrie = new Product("Brie", 3.50, $categorieFromages);
+$produitComte = new Product("Comté", 4.20, $categorieFromages);
+$produitRoquefort = new Product("Roquefort", 5.10, $categorieFromages);
+$produitEmmental = new Product("Emmental", 2.10, $categorieFromages);
+
+$quantityBrie = new quantity($produitBrie, 2);
+$quantityComte = new quantity($produitComte, 1);
+$quantityRoquefort = new quantity($produitRoquefort, 3);
+$quantityEmmental = new quantity($produitEmmental, 245);
+
+$weightBrie = new weight($produitBrie, 2.5);
+$weightComte = new weight($produitComte, 1.5);
+$weightRoquefort = new weight($produitRoquefort, 3);
+$weightEmmental = new weight($produitEmmental, 5);
+
+echo " - " . $weightBrie->weight . " Kilo - " . $quantityBrie->product->nom . " (" . $quantityBrie->product->category->nom . ") x" . $quantityBrie->quantite
+    . " = " . number_format($quantityBrie->getTotal(), 2) . " €<br>";
+
+echo " - " . $weightComte->weight . " Kilo - " . $quantityComte->product->nom . " (" . $quantityComte->product->category->nom . ") x" . $quantityComte->quantite
+    . " = " . number_format($quantityComte->getTotal(), 2) . " €<br>";
+
+echo " - " . $weightRoquefort->weight . " Kilo - " . $quantityRoquefort->product->nom . " (" . $quantityRoquefort->product->category->nom . ") x" . $quantityRoquefort->quantite
+    . " = " . number_format($quantityRoquefort->getTotal(), 2) . " €<br>";
+
+echo " - " . $weightEmmental->weight . " Kilo - " . $quantityEmmental->product->nom . " (" . $quantityEmmental->product->category->nom . ") x" . $quantityEmmental->quantite
+    . " = " . number_format($quantityEmmental->getTotal(), 2) . " €<br><br>";
+
+$quantityComte->incrementequantity(2);
+$quantityRoquefort->decrementequantity(1);
+$weightEmmental->decrementeweight(2.5);
+
+echo "Après modification :<br>";
+echo $quantityComte->product->nom . " x" . $quantityComte->quantite . " = " . number_format($quantityComte->getTotal(), 2) . " €<br>";
+echo $quantityRoquefort->product->nom . " x" . $quantityRoquefort->quantite . " = " . number_format($quantityRoquefort->getTotal(), 2) . " €<br>";
+echo $quantityEmmental->product->nom . " - " . $weightEmmental->weight . " Kilo - " . number_format($quantityRoquefort->getTotal(), 2) . " €<br>";
