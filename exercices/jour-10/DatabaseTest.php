1d5970c6f0e0699beed60005a02ebfa63624ff08
dylanholin-campus <dylan.holin@le-campus-numerique.fr>
2026-01-19 14:44:59 +0100

    Jour 10: exo 6

---
+++ exercices/jour-10/DatabaseTest.php
@@ -0,0 +1,14 @@
+<?php
+require 'Database.php';
+
+$pdo = Database::getInstance();
+
+$sql = "SELECT * FROM users";  
+$stmt = $pdo->query($sql);
+$users = $stmt->fetchAll();
+
+echo "<pre>";
+print_r($users);
+echo "</pre>";
+
+?>
